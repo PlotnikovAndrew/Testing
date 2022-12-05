815f460df379937a70df13b439d8e5e0d1bf27fd
PlotnikovAndrew <drunyy@gmail.com>
2022-12-05 20:39:00 +0300

    First commit

---
+++ src/ru/otus/HW/homework7/game/Dice.java
@@ -0,0 +1,5 @@
+package ru.otus.HW.homework7.game;
+
+public interface Dice {
+    int roll();
+}
---
+++ src/ru/otus/HW/homework7/game/GameWinnerConsolePrinter.java
@@ -0,0 +1,8 @@
+package ru.otus.HW.homework7.game;
+
+public class GameWinnerConsolePrinter implements GameWinnerPrinter {
+    @Override
+    public void printWinner(Player winner) {
+        System.out.printf("Победитель: %s%n", winner.getName());
+    }
+}
